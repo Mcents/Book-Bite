0004ff48e9442ea43ccca1d7ac4e2ff3451cc460
Michael Centrelli <Acentrelli@gmail.com>
2017-07-18 17:18:05 -0600

    Pass Tests for User Authentication -Test for the logging in and out of a user -Test for the creation of a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
 
-
   def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome, #{@user.username}"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
   end
 
 end
