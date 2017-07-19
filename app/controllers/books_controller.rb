33d62e24bda4c2516f1c76daebeb9a9a361d744e
Michael Centrelli <Acentrelli@gmail.com>
2017-07-19 15:22:15 -0600

    Delete method added for Book--Test to come

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
   private
 
   def book_params
